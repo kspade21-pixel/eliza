0416596d42104788c90c1340bfc4c158fc414621
kspade21-pixel <kspade21@gmail.com>
2026-08-24 02:06:34 -0400

    feat: export paper backtesting APIs

--- plugins/plugin-paper-trading/src/index.ts
+++ plugins/plugin-paper-trading/src/index.ts
@@ -1,3 +1,10 @@
+export {
+  DEFAULT_BACKTEST_POLICY,
+  runPaperBacktest,
+  type BacktestPolicy,
+  type BacktestResult,
+  type HistoricalPrice,
+} from "./backtest.js";
 export { formatUsdMicros, paperTradingAction } from "./action.js";
 export { DEFAULT_PAPER_POLICY, PaperTradingEngine } from "./engine.js";
 export { CoinGeckoKeylessQuoteSource } from "./market-data.js";
