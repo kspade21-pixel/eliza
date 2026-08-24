ffa7a26746433b4b5088ea0c04225c4d36f1653a
kspade21-pixel <kspade21@gmail.com>
2026-08-24 00:02:55 -0400

    feat(paper-trading): export simulator core

---
+++ plugins/plugin-paper-trading/src/index.ts
@@ -0,0 +1,14 @@
+export { DEFAULT_PAPER_POLICY, PaperTradingEngine } from "./engine.js";
+export {
+  ASSET_SCALE,
+  BPS_SCALE,
+  USD_SCALE,
+  type AuditReceipt,
+  type PaperLedger,
+  type PaperOrder,
+  type PaperPosition,
+  type PaperSide,
+  type PaperSnapshot,
+  type QuoteSnapshot,
+  type RiskPolicy,
+} from "./types.js";
